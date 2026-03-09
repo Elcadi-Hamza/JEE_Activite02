8a6b9fe73c32a59c200347f00b32267b27dff1fd
elcadiHamza <email_du_compte_rick679>
2026-03-09 12:02:45 +0000

    feat : l'ajoute des classe de controlleur et view, ProductController, et la connexion de base donnes changeant das application.properties et l'ajoute d'un view resources/templates/products.html

--- src/main/java/net/hamza/glsidensetspringmvc/GlsidEnsetSpringMvcApplication.java
+++ src/main/java/net/hamza/glsidensetspringmvc/GlsidEnsetSpringMvcApplication.java
@@ -5,9 +5,10 @@ import net.hamza.glsidensetspringmvc.repository.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class GlsidEnsetSpringMvcApplication {
 
     public static void main(String[] args) {
