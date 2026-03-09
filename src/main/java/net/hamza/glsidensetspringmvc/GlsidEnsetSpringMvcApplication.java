b9b4e2b709140fd34a4f4931f97e54f0d99c19ff
elcadiHamza <email_du_compte_rick679>
2026-03-09 10:06:09 +0000

    feat : la creation des packages entities, repository et web, et la creation de la classe Product, et fichier readme

---
+++ src/main/java/net/hamza/glsidensetspringmvc/GlsidEnsetSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package net.hamza.glsidensetspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlsidEnsetSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlsidEnsetSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/hamza/glsidensetspringmvc/GlsidEnsetSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hamza.glsidensetspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlsidEnsetSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
